b77e45f1a87d2e1784389b034a6063efb7529f0e
Paul Chavard <pro@paul.chavard.net>
2022-06-19 00:17:47 +0200

    feat(demo): add revalidation and toggle

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -1,5 +1,45 @@
-import { createBrowserTurboRouter } from 'remix-router-turbo';
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createBrowserTurboRouter, classList } from 'remix-router-turbo';
+import { Application, Controller } from '@hotwired/stimulus';
 
 import routes from '../routes.json';
 
-createBrowserTurboRouter({ routes, debug: true });
+const application = new Application();
+
+class ToggleController extends Controller {
+  static targets = ['toggle'];
+  declare readonly toggleTargets: HTMLInputElement[];
+
+  close(event: KeyboardEvent) {
+    if (event.key == 'Escape' || event.key == 'Enter') {
+      if (event.target instanceof HTMLInputElement) {
+        event.target.blur();
+        requestAnimationFrame(() => this.focusButton());
+      }
+    }
+  }
+
+  toggle() {
+    for (const element of this.toggleTargets) {
+      classList(element).toggle('hidden');
+      const input = element.querySelector<HTMLInputElement>('input[autofocus]');
+      if (input && !element.classList.contains('hidden')) {
+        this.focusInput(input);
+      }
+    }
+  }
+
+  private focusInput(input: HTMLInputElement) {
+    const length = input.value.length;
+    input.focus();
+    input.setSelectionRange(length, length);
+  }
+
+  private focusButton() {
+    this.element.querySelector<HTMLButtonElement>('button[data-toggle-target="toggle"]')?.focus();
+  }
+}
+
+application.register('toggle', ToggleController);
+
+createBrowserTurboRouter({ routes, application, debug: true });
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,11 @@
 class Todo < ApplicationRecord
   validates :title, presence: true
+
+  def validating?
+    created_at > 20.seconds.ago
+  end
+
+  def validated?
+    created_at < 10.seconds.ago
+  end
 end
